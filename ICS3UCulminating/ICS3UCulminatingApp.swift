3ebb824ffa552f0e3bf924e354fedcf6b83a14e9
Venessa Huang <vhuang@lcs.on.ca>
2026-06-01 11:27:17 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by 黃翊喬 on 2026/6/1.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
